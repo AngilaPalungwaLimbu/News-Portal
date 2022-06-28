51ff5516f2c78c01a0fd33ed9b2d576f1102fb63
AngilaPalungwaLimbu <nzelapalungwa03@gmail.com>
2022-06-28 12:45:50 +0545

    topbar

--- resources/views/frontend/app.blade.php
+++ resources/views/frontend/app.blade.php
@@ -10,11 +10,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <script src="https://kit.fontawesome.com/99694a0c52.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v14.0" nonce="fdjooq0f"></script>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v14.0"
+        nonce="fdjooq0f"></script>
+
+    <!-- Logo -->
+    @include('frontend.components.topbar')
     <!-- Logo -->
     @include('frontend.components.logo')
     <!-- Menu -->
@@ -28,6 +33,17 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
+
+    <script>
+        var frame = document.getElementById('myFrame');
+        frame.onload = function() {
+            var body = frame.contentWindow.document.querySelector('body');
+            body.style.color = 'red';
+            body.style.fontSize = '20px';
+            body.style.lineHeight = '20px';
+        };
+    </script>
 </body>
+
 
 </html>
---
+++ resources/views/frontend/components/topbar.blade.php
@@ -0,0 +1,16 @@
+<div class="container-fluid topcolor">
+    <div class="container  d-flex  justify-content-between">
+        <div>
+            <iframe id="myFrame" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" allowtransparency="true"
+                src="https://www.ashesh.com.np/linknepali-time.php?time_only=no&font_color=333333&aj_time=yes&font_size=14&line_brake=0&api=822163m095"
+                width="334" height="22" font_color=fff></iframe>
+        </div>
+
+        <div class="icons text-white">
+            <i class="fa-brands fa-facebook-square m-2"></i>
+            <i class="fa-brands fa-instagram m-2"></i>
+            <i class="fa-brands fa-linkedin m-2"></i>
+            <i class="fa-brands fa-twitter-square m-2"></i>
+        </div>
+    </div>
+</div>
