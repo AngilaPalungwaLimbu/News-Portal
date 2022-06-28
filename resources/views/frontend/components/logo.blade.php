49fdbe855904a55830556cd84329d137ac620d04
AngilaPalungwaLimbu <nzelapalungwa03@gmail.com>
2022-06-28 13:06:51 +0545

    top ad

--- resources/views/frontend/components/logo.blade.php
+++ resources/views/frontend/components/logo.blade.php
@@ -1,10 +1,21 @@
-<div class="container py-4 d-flex">
-    <a href="/">
-        <img src="{{ asset($company->logo) }}" width="45%" alt="">
-    </a>
-    @if (!empty($top_ads))
-    <a href="{{ $top_ads->link }}" target="_blank">
-        <img src="{{ asset($top_ads->image) }}"alt=""  width="100%">
-    </a>
-    @endif
+<div class="container py-4  ">
+
+    <div class="row ">
+        <div class="col-md-4">
+            <a href="/">
+                <img src="{{ asset($company->logo) }}" width="100%" alt="">
+            </a>
+        </div>
+        <div class="col-md-6 offset-md-2">
+            @if (!empty($top_ads))
+            <a href="{{ $top_ads->link }}" target="_blank">
+                <img src="{{ asset($top_ads->image) }}"alt=""  width="100%">
+            </a>
+            @endif
+        </div>
+    </div>
+
+
+
+
 </div>
